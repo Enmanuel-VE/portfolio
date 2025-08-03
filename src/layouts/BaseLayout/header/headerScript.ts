052c5230f56fc54fa9f87e5bfe38e181fab6f14a
Enmanuel Antoni Bracho Villanueva <enmanuel02estudios@gmail.com>
2025-08-03 14:06:16 -0400

    feat: Simplify header element retrieval and improve scroll handling logic

--- src/layouts/BaseLayout/header/headerScript.ts
+++ src/layouts/BaseLayout/header/headerScript.ts
@@ -1,78 +1,62 @@
 import toggleThemeHandler from "./toggleThemeHandler";
 
-const headerElementIds = {
-  HEADER_SENTINEL: "header-sentinel",
-  HEADER_LAYOUT: "header-layout",
-  HEADER_CONTAINER: "header-container",
+const HEADER_SENTINEL = document.getElementById("header-sentinel");
 
-  NAV_ACTIONS: "nav-actions",
-  THEME_TOGGLE: "theme-toggle",
-  SCROLL_PROYECTS: "scroll-to-proyects",
+const Elements = {
+  NAV_ACTIONS: document.getElementById("nav-actions"),
+  THEME_TOGGLE: document.getElementById("theme-toggle"),
+  SCROLL_PROYECTS: document.getElementById("scroll-to-proyects"),
 };
 
-const headerElements: { [key: string]: HTMLElement | null } = {};
-
-Object.entries(headerElementIds).forEach(([key, value]) => {
-  headerElements[value] = document.getElementById(value);
-});
-
-console.log(headerElements);
-
 const handleScroll = (): void => {
+  const IS_VISIBLE_STYLES = {
+    NotVisible: {
+      NAV_ACTIONS:
+        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full transition-all duration-250 ease-in-out px-4 bg-base-100/50 backdrop-blur-md shadow-4xl border-white/15",
+
+      THEME_TOGGLE:
+        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full bg-base-100/50 backdrop-blur-md shadow-4xl border-white/15 transition-all duration-250 ease-in-out px-2 cursor-pointer",
+      SCROLL_PROYECTS:
+        "transition-all duration-250 ease-in-out absolute bottom-4 left-1/2 transform -translate-x-1/2 opacity-0 cursor-default pointer-events-none",
+    },
+    visible: {
+      NAV_ACTIONS:
+        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full transition-all duration-250 ease-in-out px-4",
+      THEME_TOGGLE:
+        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full border-primary/0 transition-all duration-250 ease-in-out px-2 cursor-pointer",
+      SCROLL_PROYECTS:
+        "transition-all duration-250 ease-in-out absolute bottom-4 left-1/2 transform -translate-x-1/2",
+    } as const,
+  };
+
   const observer = new IntersectionObserver((entries) => {
     const sentinelIsVisible: boolean = entries[0].isIntersecting;
 
     if (!sentinelIsVisible) {
-      headerElements["nav-actions"]!.classList.remove("border-primary/0");
-      headerElements["theme-toggle"]!.classList.remove("border-primary/0");
+      Elements.NAV_ACTIONS!.className =
+        IS_VISIBLE_STYLES.NotVisible.NAV_ACTIONS;
 
-      headerElements["nav-actions"]!.classList.add(
-        "bg-base-100/50",
-        "backdrop-blur-md",
-        "shadow-4xl",
-        "border-white/15",
-      );
-      headerElements["theme-toggle"]!.classList.add(
-        "bg-base-100/50",
-        "backdrop-blur-md",
-        "shadow-4xl",
-        "border-white/15",
-      );
-      headerElements["scroll-to-proyects"]!.classList.add(
-        "opacity-0",
-        "cursor-default",
-        "pointer-events-none",
-      );
+      Elements.SCROLL_PROYECTS!.className =
+        IS_VISIBLE_STYLES.NotVisible.SCROLL_PROYECTS;
+
+      Elements.THEME_TOGGLE!.className =
+        IS_VISIBLE_STYLES.NotVisible.THEME_TOGGLE;
     } else {
-      headerElements["nav-actions"]!.classList.add("border-primary/0");
-      headerElements["theme-toggle"]!.classList.add("border-primary/0");
+      Elements.NAV_ACTIONS!.className = IS_VISIBLE_STYLES.visible.NAV_ACTIONS;
 
-      headerElements["nav-actions"]!.classList.remove(
-        "bg-base-100/50",
-        "backdrop-blur-md",
-        "shadow-4xl",
-        "border-white/15",
-      );
-      headerElements["theme-toggle"]!.classList.remove(
-        "bg-base-100/50",
-        "backdrop-blur-md",
-        "shadow-4xl",
-        "border-white/15",
-      );
-      headerElements["scroll-to-proyects"]!.classList.remove(
-        "opacity-0",
-        "cursor-default",
-        "pointer-events-none",
-      );
+      Elements.SCROLL_PROYECTS!.className =
+        IS_VISIBLE_STYLES.visible.SCROLL_PROYECTS;
+
+      Elements.THEME_TOGGLE!.className = IS_VISIBLE_STYLES.visible.THEME_TOGGLE;
     }
   });
-  observer.observe(headerElements["header-sentinel"]!);
+  observer.observe(HEADER_SENTINEL!);
 };
 
 const main = (): void => {
   const themeToggleButton = document.getElementById("theme-change");
 
-  themeToggleButton?.addEventListener("click", toggleThemeHandler);
+  themeToggleButton!.addEventListener("click", toggleThemeHandler);
   handleScroll();
 };
 
