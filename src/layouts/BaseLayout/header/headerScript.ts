6ca40cff5d86b775bff142e9d956dbfcff09d501
Enmanuel Antoni Bracho Villanueva <enmanuel02estudios@gmail.com>
2025-08-04 15:19:28 -0400

    feat: Refactor project components and structure, including new ScrollToProyects and ContactButton components, and update theme handling logic

--- src/layouts/BaseLayout/header/headerScript.ts
+++ src/layouts/BaseLayout/header/headerScript.ts
@@ -1,5 +1,3 @@
-import toggleThemeHandler from "./toggleThemeHandler";
-
 const HEADER_SENTINEL = document.getElementById("header-sentinel");
 
 const Elements = {
@@ -8,27 +6,27 @@ const Elements = {
   SCROLL_PROYECTS: document.getElementById("scroll-to-proyects"),
 };
 
+const IS_VISIBLE_STYLES = {
+  NotVisible: {
+    NAV_ACTIONS:
+      "flex flex-row justify-center items-center border-1 border-base-content/25 rounded-full bg-base-100/50 backdrop-blur-md transition-all duration-250 ease-in-out px-4 shadow-4xl ",
+
+    THEME_TOGGLE:
+      "flex flex-row justify-center items-center border-1 border-base-content/25 rounded-full bg-base-100/50 backdrop-blur-md transition-all duration-250 ease-in-out px-2 shadow-4xl cursor-pointer",
+    SCROLL_PROYECTS:
+      "transition-all duration-250 ease-in-out absolute bottom-4 left-1/2 transform -translate-x-1/2 opacity-0 cursor-default pointer-events-none",
+  },
+  visible: {
+    NAV_ACTIONS:
+      "flex flex-row justify-center items-center border-1 border-base-content/0 rounded-full transition-all duration-250 ease-in-out px-4",
+    THEME_TOGGLE:
+      "flex flex-row justify-center items-center border-1 border-base-content/0 rounded-full transition-all duration-250 ease-in-out px-2 cursor-pointer",
+    SCROLL_PROYECTS:
+      "transition-all duration-250 ease-in-out absolute bottom-4 left-1/2 transform -translate-x-1/2",
+  } as const,
+};
+
 const handleScroll = (): void => {
-  const IS_VISIBLE_STYLES = {
-    NotVisible: {
-      NAV_ACTIONS:
-        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full transition-all duration-250 ease-in-out px-4 bg-base-100/50 backdrop-blur-md shadow-4xl border-white/15",
-
-      THEME_TOGGLE:
-        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full bg-base-100/50 backdrop-blur-md shadow-4xl border-white/15 transition-all duration-250 ease-in-out px-2 cursor-pointer",
-      SCROLL_PROYECTS:
-        "transition-all duration-250 ease-in-out absolute bottom-4 left-1/2 transform -translate-x-1/2 opacity-0 cursor-default pointer-events-none",
-    },
-    visible: {
-      NAV_ACTIONS:
-        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full transition-all duration-250 ease-in-out px-4",
-      THEME_TOGGLE:
-        "flex flex-row justify-center items-center border-1 border-primary/0 rounded-full border-primary/0 transition-all duration-250 ease-in-out px-2 cursor-pointer",
-      SCROLL_PROYECTS:
-        "transition-all duration-250 ease-in-out absolute bottom-4 left-1/2 transform -translate-x-1/2",
-    } as const,
-  };
-
   const observer = new IntersectionObserver((entries) => {
     const sentinelIsVisible: boolean = entries[0].isIntersecting;
 
@@ -53,11 +51,4 @@ const handleScroll = (): void => {
   observer.observe(HEADER_SENTINEL!);
 };
 
-const main = (): void => {
-  const themeToggleButton = document.getElementById("theme-change");
-
-  themeToggleButton!.addEventListener("click", toggleThemeHandler);
-  handleScroll();
-};
-
-window.addEventListener("load", main);
+window.addEventListener("load", handleScroll);
