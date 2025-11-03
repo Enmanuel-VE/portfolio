133de27f64f641459126282cbd776a14580a6854
Enmanuel Antoni Bracho Villanueva <enmanuelbracho.dev@gmail.com>
2025-11-03 18:58:05 -0400

    feat: Implement 404 page and improve user interface
    This commit introduces a custom 404 page to enhance the user experience when a page is not found.
    
    Main changes:
    - Created the  layout and the  page.
    - Added the logo image to the assets for use on the 404 page.
    
    Improvements and fixes:
    - Increased text opacity in dark mode to improve readability in several components.
    - Updated pnpm version to 'latest' in the deployment workflow to ensure the latest version is used.
    - Corrected a self-closing tag in .

--- src/data/images.ts
+++ src/data/images.ts
@@ -3,11 +3,13 @@ import type { ImageMetadata } from 'astro';
 import bentoGridImage from '../assets/images/bentoGridProject.png';
 import calculatorImage from '../assets/images/calculatorProject.png';
 import profileImage from '../assets/images/cat.jpg';
+import logoImage from '../assets/images/logo.png';
 
 const images: Record<string, ImageMetadata> = {
 	bentoGridImage,
 	calculatorImage,
 	profileImage,
+	logoImage,
 };
 
 export default images;
